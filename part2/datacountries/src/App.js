f6da429fb181ebab2b2eb02bbd356f1a17eb0597
Maria De La Rosa <victy-88@hotmail.com>
2021-07-24 07:18:52 -0400

    feat: add filter for countries

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -3,18 +3,43 @@ import axios from 'axios'
 
 const App = () => {
   const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
   useEffect(() => {
     axios.
     get('https://restcountries.eu/rest/v2/all').
     then(response =>{
       setCountries(response.data)
-      console.log(countries)
     },[])
   })
 
+  const handleFilter = (e) =>{
+    const getValue = e.target.value
+    setFilter(getValue)
+  }
+
+  
+    const filterCountriesNames  = countries.filter((country) => {
+      if(filter === ''){
+        return country.name
+      } else if (country.name.toLowerCase().includes(filter.toLowerCase())){
+          if(country.length < 10){
+            return country.name
+          } else {
+            return <p>Too many matches, specify another filter</p>
+          }
+      }
+      console.log(country.length)
+    })
+
   return (
     <div>
-     
+      <div>
+        <input value={filter} onChange={handleFilter} />
+      </div>
+      <ul>
+        {filterCountriesNames.map(country => <li>{country.name}</li>)}
+      </ul>
     </div>
   );
 }
