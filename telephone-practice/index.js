b9dd598e2d51ae839987b00660b25ba4f8d7a908
m.delarosa <m.delarosa@gbh.com.do>
2021-09-19 10:24:10 -0400

    WIP: add telephone practice

--- telephone-practice/index.js
+++ telephone-practice/index.js
@@ -1,4 +1,8 @@
-const person = [
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+let person = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -21,4 +25,63 @@ const person = [
     }
 ]
 
-console.log('n')
+app.get('/api/person', (request, response)=>{
+  response.send(person)
+})
+
+app.get('/api/person/:id', (request, response) => {
+  const id = Number(request.params.id)
+  note = person.find(note => note.id === id)
+
+  if(note) {
+    response.json(note)
+  }else{
+    response.statusMessage= "There is not note with that ID" 
+    response.status(404).end()
+  }
+})
+
+const idGenerator = () => {
+  let id = Math.floor(Math.random()*10000)
+  return id
+}
+
+
+app.post('/api/person', (request, response)=> {
+  const body = request.body
+
+  let repeatedName = person.find( name => name.name === body.name)
+
+  if(!body.name){
+    return(response.status(400).json({
+      error:"name is missing"
+    }))
+  } else if(!body.number){
+    return(response.status(400).json({
+      error:"number is missing"
+    }))
+  } else if(repeatedName){
+    return(response.status(404).json({
+      error:"name must be unique"
+    }))
+  }
+
+  const byperson = {
+    id:idGenerator(),
+    name:body.name,
+    number: body.number
+  }
+
+  person = person.concat(byperson)
+  response.json(byperson)
+
+})
+
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phone has info  for ${person.length} people </p>`)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`running on PORT ${PORT}`)
