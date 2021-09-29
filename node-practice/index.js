f45fb69b76270c1e6e09244d43edac256a7a9866
m.delarosa <m.delarosa@gbh.com.do>
2021-09-29 10:38:24 -0400

    feat: add node handling error

--- node-practice/index.js
+++ node-practice/index.js
@@ -1,89 +1,84 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/notes')
 const app = express()
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
 
-  app.get('/api/notes', (request, response) => {
-    response.send(notes)
-  })
-
-  app.get('/api/notes/:id', (request, response) => {
-      const id = Number(request.params.id)
-      const note = notes.find(note => note.id === id)
-      
-      if(note) {
-        response.json(note)
-      }else {
-        response.statusMessage ="There is not note with that ID" 
-        response.status(404).end()
-      }
-
-  })
-
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-
-  })
-
-  const generateId = () => {
-    const maxId =  notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-
-    return maxId + 1
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  } else if(error.name === 'ValidationError') {
+    return(response.status(400).json({error: error.message}))
   }
 
-    app.post('/api/notes', (request, response) => {
-      const body =  request.body
+  next(error)
+}
 
-      if(!body.content) {
-        return response.status(400).json({
-          error: 'content missing'
-        })
-      }
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes =>{
+    response.json(notes)
+  })
+})
 
-      const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId()
-      }
-
-      notes = notes.concat(note)
-
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id).then(note =>{
+    if(note){
       response.json(note)
+    } else {
+      response.status(404).end()
+    }
+    
+  })
+  .catch(error => (next(error)))
+})
 
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body =  request.body
+
+  const note = new Note ({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
   })
 
 
+  note.save().then(savedNote => {
+    response.json(savedNote.toJSON)
+  })
+  .catch(error => next(error))
+
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new:true})
+  .then(updateNote => {
+    response.json(updateNote)
+  })
+  .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
---
+++ node-practice/mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+if(process.env.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://user_admin:${password}@notes.epon4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema =  new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+    content: 'Mongoose makes the use of mongo easy',
+    date: new Date(),
+    important: true,
+})
+
+note.save().then( result =>{
+    console.log('note saved')
+    mongoose.connection.close()
+})
+
+Note.find({}).then(result =>{
+    result.forEach( note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
