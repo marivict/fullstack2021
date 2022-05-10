51d73e2c778d41635d28720bd6e3cdc050f709ae
m.delarosa <m.delarosa@gbh.com.do>
2022-05-10 18:36:46 -0400

    feat: add test to the node aplication

--- node-practice/controller/notes.js
+++ node-practice/controller/notes.js
@@ -1,23 +1,23 @@
-const notes = require('../models/notes')
 const Note = require('../models/notes')
 
 const notesRouter = require('express').Router()
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then((note) => {
-    response.json(note)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      response.json(note)
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response) => {
+
+  const note = await Note.findById(request.params.id)
+  if(note) {
+    response.json(note.toJSON())
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -25,32 +25,34 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
     date: new Date
   })
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
+
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+
+})
+
+// note.save()
+//   .then(savedNote => {
+//     response.status(201).json(savedNote)
+//   })
+//   .catch(error => next(error))
+
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updateNote => {
+      response.json(updateNote)
     })
     .catch(error => next(error))
-
-  notesRouter.delete('/:id', (request, response, next) => {
-    Note.findIdAndRemove(request.params.id)
-      .then(() => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
-
-  notes.Router.put('/:id', (request, response, next) => {
-    const body = request.body
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updateNote => {
-        response.json(updateNote)
-      })
-      .catch(error => next(error))
-  })
 })
 
 module.exports = notesRouter
--- notes/src/services/notes.js
+++ notes/src/services/notes.js
@@ -3,14 +3,14 @@ const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
-    const nonExistingNote = {
-        id:1000,
-        content: 'This note is not saved to server',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    }
+    // const nonExistingNote = {
+    //     id:1000,
+    //     content: 'This note is not saved to server',
+    //     date: '2019-05-30T17:30:31.098Z',
+    //     important: true,
+    // }
 
-    return request.then(response => response.data.concat(nonExistingNote))
+    return request.then(response => response.data)
 }
 
 const create = newObject => {
